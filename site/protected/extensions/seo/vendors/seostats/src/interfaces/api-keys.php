32168a64e0150c6244c211c38c6160f5f3500041
Will Smelser <willsmelser@gmail.com>
2013-10-20 11:10:42 -0400

    Ok, got this removed form index.

--- site/protected/extensions/seo/vendors/seostats/src/interfaces/api-keys.php
+++ site/protected/extensions/seo/vendors/seostats/src/interfaces/api-keys.php
@@ -10,6 +10,8 @@
 interface api_keys
 {
     //const VENDOR_APP_ID = 'FCB316D398F094C53B8C7AA8DA24B2D58AB520F8';
+    const GOOGLE_CX = '018110159205303224694:lc6g5a3yxzw';
+    const GOOGLE_KEY = 'AIzaSyA_wkenQWzwHcxuaozcfIc_gcuMo9E09TM';
 }
 
 /* End of file api-keys.php */

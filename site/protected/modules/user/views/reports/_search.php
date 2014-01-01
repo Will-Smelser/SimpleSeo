103f2be7612f0d88ddebd578a77787c7b168dd9a
Will-Smelser <Will1480>
2014-01-01 16:55:32 -0500

    Forgot these views are needed.

---
+++ site/protected/modules/user/views/reports/_search.php
@@ -0,0 +1,50 @@
+<?php
+/* @var $this ReportdataController */
+/* @var $model Reportdata */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id',array('size'=>20,'maxlength'=>20)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'data'); ?>
+		<?php echo $form->textField($model,'data'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'domain'); ?>
+		<?php echo $form->textField($model,'domain',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'uri'); ?>
+		<?php echo $form->textField($model,'uri',array('size'=>60,'maxlength'=>2048)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'created'); ?>
+		<?php echo $form->textField($model,'created'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
---
+++ site/protected/modules/user/views/reports/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this ReportdataController */
+/* @var $data Reportdata */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('data')); ?>:</b>
+	<?php echo CHtml::encode($data->data); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('user_id')); ?>:</b>
+	<?php echo CHtml::encode($data->user_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('domain')); ?>:</b>
+	<?php echo CHtml::encode($data->domain); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('uri')); ?>:</b>
+	<?php echo CHtml::encode($data->uri); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created')); ?>:</b>
+	<?php echo CHtml::encode($data->created); ?>
+	<br />
+
+
+</div>
